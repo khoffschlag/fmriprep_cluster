8a0f3cb12ac84ec6324ee3d14a7175681dd9f830
Kevin Hoffschlag <72939508+khoffschlag@users.noreply.github.com>
2025-08-19 00:15:11 +0200

    fetch current username and use it as folder name

--- fmriprep_wrapper.sh
+++ fmriprep_wrapper.sh
@@ -71,7 +71,7 @@ echo "*************************************************************"
 echo "Starting on \$(hostname) at \$(date +"%T")"
 echo "*************************************************************"
 
-TMP_LOCAL="/local/work/renglert/A01/${PARTICIPANT_ID}"
+TMP_LOCAL="/local/work/$(whoami)/A01/${PARTICIPANT_ID}"
 echo "\${TMP_LOCAL}"
 
 # directory for single sub BIDS
